106f99188f903855871e3109f05545a49280f4d1
gimtonic <gimtonic@rambler.ru>
2018-03-13 05:28:59 -0700

    Create and update Category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -11,7 +11,7 @@
     @endcomponent
 
             <hr>
-            <a href="{{route('admin.category.index')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i> Создать категорию</a>
+            <a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fa fa-plus-square-o"></i> Создать категорию</a>
             <table class="table table-striped">
                 <thead>
                 <th>Наименование</th>
@@ -24,7 +24,7 @@
                     <td>{{$category->title}}</td>
                     <td>{{$category->published}}</td>
                     <td>
-                        <a href="{{route('admin.category.edit', ['id'=>$category->id])}}"><i class="fa fa-edit"></i></a>
+                        <a href="{{route('admin.category.edit',$category)}}"><i class="fa fa-edit"></i></a>
                     </td>
                 </tr>
                 @empty
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -28,7 +28,7 @@
         <!--Два блока с отображениями -->
         <div class="row">
             <div class="col-sm-6">
-                <a class="btn btn-block btn-default" href="#">Создать категорию</a>
+                <a class="btn btn-block btn-default" href="{{route('admin.category.create')}}">Создать категорию</a>
                 <a href="#" class="list-group-item">
                     <h4 class="list-group-item-heading">Категория первая</h4>
                     <p class="list-group-item-text">Кол-во материалов</p>
